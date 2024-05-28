36a441794a64a2f477938416ac95fefc2b57e1bc
vmathur <vmathur@users.noreply.github.com>
2024-05-27 17:29:33 -0700

    wrapped in suspense boundary

--- app/user/page.js
+++ app/user/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Suspense } from 'react';
 import GameHistory from '../components/GameHistory'
 import Avatar from 'boring-avatars';
 import styles from '../page.module.css';
@@ -32,23 +32,25 @@ const User = () => {
   }, [web3]);
 
   return (
-    <div>
-      <div className={styles.userContainer}>
-        <div className={styles.profileContainer}>
-          <Avatar
-                size={40}
-                name={address}
-                variant="beam"
-                colors={colors}
-          />
-          <div className={styles.userAddress}>{address}</div>
-          <div>&nbsp;&nbsp;{score} points</div>
-        </div>
-        <div className={styles.gameHistoryContainer}>
-          {completedGames.length>0 ? <GameHistory address={address} games={completedGames}/> : ''}
+    <Suspense>
+      <div>
+        <div className={styles.userContainer}>
+          <div className={styles.profileContainer}>
+            <Avatar
+                  size={40}
+                  name={address}
+                  variant="beam"
+                  colors={colors}
+            />
+            <div className={styles.userAddress}>{address}</div>
+            <div>&nbsp;&nbsp;{score} points</div>
+          </div>
+          <div className={styles.gameHistoryContainer}>
+            {completedGames.length>0 ? <GameHistory address={address} games={completedGames}/> : ''}
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
